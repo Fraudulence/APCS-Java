e0b626407c4a5b95ada1e7444292e2b1c59a04df
Fraudulence <47933368+Fraudulence@users.noreply.github.com>
2019-04-29 12:50:44 -0400

    Add files via upload

--- Dog Test/DogTest.java
+++ Dog Test/DogTest.java
@@ -11,10 +11,10 @@ public class DogTest
         Dog dog = new Dog("Spike");
         System.out.println(dog.getName() + " says " + dog.speak());
         
-        Labrador lab = new Labrador("Joker", "black");
-        System.out.println(dog.getName() + " says " + dog.speak());
+        Dog lab = new Labrador("Joker", "black");
+        System.out.println(lab.getName() + " says " + lab.speak());
         
-        Yorkshire york = new Yorkshire("Morgana");
-        System.out.println(dog.getName() + " says " + dog.speak());
+        Dog york = new Yorkshire("Morgana");
+        System.out.println(york.getName() + " says " + york.speak());
     }
 }
