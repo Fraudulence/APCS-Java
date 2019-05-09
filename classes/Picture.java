a1da0d58f331d1ec83ac282642818382e3793a61
Fraudulence <47933368+Fraudulence@users.noreply.github.com>
2019-05-09 14:17:49 -0400

    Add files via upload

--- classes/Picture.java
+++ classes/Picture.java
@@ -75,6 +75,23 @@ public class Picture extends SimplePicture
         }
     }
     
+    public void mirrorVerticalRightToLeft()
+    {
+        Pixel[][] pixels = this.getPixels2D();
+        Pixel leftPixel = null;
+        Pixel rightPixel = null;
+        int width = pixels[0].length;
+        for (int row = 0; row < pixels.length; row++)
+        {
+            for (int col = 0; col < width / 2; col++)
+            {
+                leftPixel = pixels[row][col];
+                rightPixel = pixels[row][width - 1 - col];
+                rightPixel.setColor(leftPixel.getColor());
+            }
+        } 
+    }
+    
     /**
      * Constructor that takes a file name and creates the picture 
      * @param fileName the name of the file to create the picture from
